21577c1cea7dcf62cab57f66257324be9a287eae
KumarAmitt <amitt2015@outlook.com>
2021-02-12 19:18:13 +0530

    Add validation and foreign keys to comments table

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,9 @@
 class Comment < ApplicationRecord
+
+  belongs_to :post
+  belongs_to :user
+
+  validates :commentator, presence: true
+  validates :comment, presence: true, length: { in: 1..200 }
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_12_095721) do
+ActiveRecord::Schema.define(version: 2021_02_12_125331) do
 
   create_table "comments", force: :cascade do |t|
     t.string "commentator"
     t.text "comment"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.integer "post_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
