b57c6f73c9a715bfe6109ed7d56612744ecf078b
ZawadiSumba66 <rosesumba98@gmail.com>
2021-02-12 12:42:50 +0300

    Add the validations for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
 
-  
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true, length: { in: 4..12 }
+  validates :password, presence: true, length: { in: 6..16}
 end
