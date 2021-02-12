0b7f18eb09bb2d01ae9b163f41d395f953b400a7
KumarAmitt <amitt2015@outlook.com>
2021-02-12 14:47:57 +0530

    Geneate model for User

--- db/migrate/20210212090545_create_users.rb
+++ db/migrate/20210212090545_create_users.rb
@@ -1,8 +1,9 @@
-class CreateAuthors < ActiveRecord::Migration[6.1]
+class CreateUsers < ActiveRecord::Migration[6.1]
   def change
-    create_table :authors do |t|
+    create_table :users do |t|
       t.string :name
       t.string :email
+      t.string :username
       t.string :password
 
       t.timestamps
